541d395626af18ba7284388bc505a8ddf73118c2
Misael da Silva Santos <misael.santos@serpro.gov.br>
2020-05-09 09:20:19 -0300

    Refatoracao do codigo

--- compras-001/compras-001.ino
+++ compras-001/compras-001.ino
@@ -11,9 +11,9 @@
 #include <MFRC522.h> //biblioteca responsável pela comunicação com o módulo RFID-RC522
 #include <SPI.h> //biblioteca para comunicação do barramento SPI
 
-#define WIFISSID "SSSSSSSSSSSSSSSSSSS" // Put your WifiSSID here
-#define PASSWORD "PPPPPPPPPPPPPPPPPPP" // Put your wifi password here
-#define TOKEN "BBBBBBBBBBBBBBBBBBBBBBBB" // Put your Ubidots' TOKEN
+#define WIFISSID "Simony_2G" // Put your WifiSSID here
+#define PASSWORD "4F63B89E" // Put your wifi password here
+#define TOKEN "BBFF-TXBx9JmZ5yjlpmmxOxzvysbIVirNRC" // Put your Ubidots' TOKEN
 #define MQTT_CLIENT_NAME "rfid01" // MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string; 
                                            //it should be a random and unique ascii string and different from all other devices
 /****************************************
@@ -22,8 +22,8 @@
 // MQTT
 #define VARIABLE_LABEL "sensor" // Assing the variable label
 #define DEVICE_LABEL "esp32" // Assig the device label
-
-char mqttBroker[]  = "industrial.api.ubidots.com";
+#define TOPIC_DEVICES "/v1.6/devices/"
+char MQTTBROKER[]  = "industrial.api.ubidots.com";
 char payload[100];
 char topic[150];
 // Space to store values to send
@@ -35,12 +35,14 @@ char str_sensor[10];
 #define SIZE_BUFFER     18
 #define MAX_SIZE_BLOCK  16
 
-// Indicadores
-#define pinVerde     12
-#define pinVermelho  32
+// LED Indicadores
+#define GREEN_LED   12
+#define RED_LED     32
+
+#define ERRO "Erro"
 
 // Indicador de sentido do estoque (0-remover, 1-adicionar)
-String indicadorEstoque="0";
+String stockMode="0";
 
 //Esse objeto 'chave' é utilizado para autenticação dos dispositivos RFID
 MFRC522::MIFARE_Key key;
@@ -50,11 +52,18 @@ MFRC522::StatusCode status;
 // Definicoes pino modulo RC522
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 
+WiFiClient wifi;
+PubSubClient client(wifi);
+
 /****************************************
  * Auxiliar Functions
  ****************************************/
-WiFiClient ubidots;
-PubSubClient client(ubidots);
+void blinkLed(int led) {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
 
 void callback(char* topic, byte* payload, unsigned int length) {
   char p[length + 1];
@@ -65,14 +74,14 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println(topic);
 }
 
-void reconnect() {
+void reconnectMQTT() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.println("Tentando conexão MQTT...");
+    Serial.println("Conectando cliente MQTT...");
     
     // Attemp to connect
     if (client.connect(MQTT_CLIENT_NAME, TOKEN, "")) {
-      Serial.println("Conectao");
+      Serial.println("Conectado");
     } else {
       Serial.print("Falha, rc=");
       Serial.print(client.state());
@@ -86,8 +95,8 @@ void reconnect() {
 //faz a leitura dos dados do cartão/tag
 String leituraDadosRFID() {
 
-  String ERRO = "Erro";
   //imprime os detalhes tecnicos do cartão/tag
+  Serial.println();
   mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); 
 
   //Prepara a chave - todas as chaves estão configuradas para FFFFFFFFFFFFh (Padrão de fábrica).
@@ -103,28 +112,22 @@ String leituraDadosRFID() {
   //faz a autenticação do bloco que vamos operar
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, bloco, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
   if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
+    Serial.print(F("Autenticacao RFID falhou: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
-    digitalWrite(pinVermelho, HIGH);
-    delay(1000);
-    digitalWrite(pinVermelho, LOW);
+    blinkLed(RED_LED);
     return (ERRO);
   }
 
   //faz a leitura dos dados do bloco
   status = mfrc522.MIFARE_Read(bloco, buffer, &tamanho);
   if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Reading failed: "));
+    Serial.print(F("Leitura RFID falhou: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
-    digitalWrite(pinVermelho, HIGH);
-    delay(1000);
-    digitalWrite(pinVermelho, LOW);
+    blinkLed(RED_LED);
     return (ERRO);
   }
   else {
-      digitalWrite(pinVerde, HIGH);
-      delay(1000);
-      digitalWrite(pinVerde, LOW);
+    blinkLed(GREEN_LED);
   }
 
   String str = (char*)buffer;
@@ -137,60 +140,72 @@ String leituraDadosRFID() {
   
 }
 
-
-void informarProduto(String produto) {
+void productSet(String produto) {
   if (!client.connected()) {
-    reconnect();
+    reconnectMQTT();
   }
   // Registrando valor do Produto
   sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
   sprintf(payload, "%s", ""); // Cleans the payload
   sprintf(payload, "{\"%s\":", produto); // Adds the variable label
-  sprintf(payload, "%s {\"value\": %s}}", payload, indicadorEstoque); // Adds the value
-  Serial.println("Publicando dados na Ubidots Cloud");
+  sprintf(payload, "%s {\"value\": %s}}", payload, stockMode); // Adds the value
+  
+  Serial.println("Enviando dados via MQTT");
   client.publish(topic, payload);
   
-  client.loop();
+  // client.loop();
   delay(1000);
   
 }
 
-void setup() {
-  // Inicia a serial
-  Serial.begin(9600);
-  SPI.begin(); // Init SPI bus
-
-  pinMode(pinVerde, OUTPUT);
-  pinMode(pinVermelho, OUTPUT);
-
-  // Wifi...
-  //Serial.begin(115200);
+void setupWifi() {
+  Serial.begin(115200);
   WiFi.begin(WIFISSID, PASSWORD);
   Serial.println();
   Serial.print("Aguardando WiFi..."); 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(500);
-  }  
+    blinkLed(RED_LED);
+  }
   Serial.println("");
   Serial.println("WiFi Conectado");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP()); 
+  blinkLed(GREEN_LED);
+}
 
-  // Inicia MQTT Client...
-  client.setServer(mqttBroker, 1883);
+void setupMQTT() {
+  SPI.begin(); // Init SPI bus
+  client.setServer(MQTTBROKER, 1883);
   client.setCallback(callback);  
-  
-  // Inicia MFRC522
+}
+
+void setupRFID() {
   mfrc522.PCD_Init(); 
   // Mensagens iniciais no serial monitor
   Serial.println();
   Serial.println("Aproxime o seu cartao do leitor...");
-  Serial.println();
+  Serial.println();  
+}
+
+void setup() {
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  // Wifi...
+  setupWifi();
+  // Inicia MQTT Client...
+  setupMQTT();   
+  // Inicia MFRC522
+  setupRFID();
 
 }
 
 void loop() {
+
+  if (!client.connected()) {
+    reconnectMQTT();
+  }
+  client.loop();
 
    // Aguarda a aproximacao do cartao
   if (!mfrc522.PICC_IsNewCardPresent()) {
@@ -201,27 +216,30 @@ void loop() {
     return;
   }
 
-
   // Tentando ler o bloco 1 da tag
-  String card = leituraDadosRFID();
+  String tag = leituraDadosRFID();
 
   // Verifica se eh produto ou admin
-  if (card != "admin") {
-    Serial.print(F("\nProduto: "));
-    Serial.println(card);
+  if (tag != "admin" && tag != ERRO) {
 
-    informarProduto(card);
+    Serial.print(F("\nProduto: "));
+    Serial.println(tag);
+    Serial.println();
+
+    productSet(tag);
+
   } else {
+
     Serial.print(F("\nCartão Administrador!"));
     //sleep(1000);
-    if (indicadorEstoque == "0") {
-      indicadorEstoque = "1";
+    if (stockMode == "0") {
+      stockMode = "1";
       Serial.print(F("\nReabastecendo!"));
     } else {
-      indicadorEstoque = "0";
+      stockMode = "0";
       Serial.print(F("\nMonitoração de estoque!"));
     }
-    informarProduto("modo");
+    productSet("modo");
   }
  
   // instrui o PICC quando no estado ACTIVE a ir para um estado de "parada"
---
+++ links.md
@@ -0,0 +1,13 @@
+
+
+# Exemplo de cliente python MQTT
+https://www.embarcados.com.br/raspberry-pi-3-na-iot-mqtt-e-python/
+
+# Exemplo de syntaxes de pesquisa
+https://www.embarcados.com.br/mqtt-protocolos-para-iot/
+
+# Instalação e segurança no mosquitto
+https://pplware.sapo.pt/tutoriais/mqtt-no-seu-raspberry-pi/
+
+# ArduinoJSON
+https://techtutorialsx.com/2017/04/26/esp32-parsing-json/
